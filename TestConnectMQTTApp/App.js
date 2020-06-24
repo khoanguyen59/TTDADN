09d4d39ab6d1f6bff40ec3312656f45922bdd21e
khoanguyen59 <phuongkhoa95ntp@gmail.com>
2020-06-24 18:52:03 +0700

    Update App.js

--- TestConnectMQTTApp/App.js
+++ TestConnectMQTTApp/App.js
@@ -1,57 +1,61 @@
-import React,{useEffect} from 'react'
-import {View, StyleSheet, Text, Button} from 'react-native'
-import MQTTConnection from './src/MQTTConnection'
-import {Buffer} from 'buffer'
+import React, {useEffect} from 'react';
+import {View, StyleSheet, Text, Button} from 'react-native';
+import MQTTConnection from './MQTTConnection';
+import {Buffer} from 'buffer';
 global.Buffer = Buffer;
 
+export default function App() {
 
-export default function App(){
-  useEffect(()=>{
-    this.mqttConnect = new MQTTConnection()
-    this.mqttConnect.onMQTTConnect = this.onMQTTConnect
-    this.mqttConnect.onMQTTLost = this.onMQTTLost
-    this.mqttConnect.onMQTTMessageArrived = this.onMQTTMessageArrived
-    this.mqttConnect.onMQTTMessageDelivered = this.onMQTTMessageDelivered
+  useEffect(() => {
+    this.mqttConnect = new MQTTConnection();
+    this.mqttConnect.onMQTTConnect = this.onMQTTConnect;
+    this.mqttConnect.onMQTTLost = this.onMQTTLost;
+    this.mqttConnect.onMQTTMessageArrived = this.onMQTTMessageArrived;
+    this.mqttConnect.onMQTTMessageDelivered = this.onMQTTMessageDelivered;
 
-    this.mqttConnect.connect('broker.hivemq.com', 8000);
+    this.mqttConnect.connect('broker.mqttdashboard.com', 8000);
 
-    onMQTTConnect = ()=>{
-      console.log('connected');
-      this.mqttConnect.subscribeChannel('testChannel');
-    }
-    onMQTTLost= ()=> {
-      console.log('connect lost')
-    }
-    onMQTTMessageArrived = (message) => {
-      console.log('onMQTTMessageArrived : ', message);
-      console.log('onMQTTMessageArrived payloadString: ', message.payloadString);
-    }
-    onMQTTMessageDelivered = (message) => {
-      console.log('onMQTTMessageDelivered : ', message);
-    }
+    onMQTTConnect = () => {
+      console.log('App onMQTTConnect');
+      this.mqttConnect.subscribeChannel('khoa');
+    };
 
-    return ()=>{
-      this.connect.close();
-    }
+    onMQTTLost = () => {
+      console.log('App onMQTTLost');
+    };
 
-  }, [])
+    onMQTTMessageArrived = message => {
+      console.log('App onMQTTMessageArrived', message);
+      console.log(
+        'App onMQTTMessageArrived payLoadString: ',
+        message.payloadString,
+      );
+    };
+    
+    onMQTTMessageDelivered = message => {
+      console.log('App onMQTTMessageDelivered', message);
+    };
 
+    return () => {
+      this.mqttConnect.close();
+    };
+  }, []);
 
   return (
-    <View style = {stypes.container}>
-      <Text>"hello"</Text>
+    <View styles={styles.container}>
+      <Text>react_native_mqtt</Text>
       <Button
-        title = "send signal"
-        onPress ={()=> this.mqttConnect.send('testChannel', "message")}
+        title="Press me"
+        onPress={() => this.mqttConnect.send('khoa', "Hello from the other side")}
       />
     </View>
-  )
+  );
 }
 
-const stypes = StyleSheet.create({
-  container:{
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+    justifyContent: 'center',
+  },
+});
