908f4de267b14e550979d9c208bcf8626158eea8
khoanguyen59 <phuongkhoa95ntp@gmail.com>
2020-06-25 19:50:29 +0700

    UI for demo

--- TestConnectMQTTApp/App.js
+++ TestConnectMQTTApp/App.js
@@ -1,11 +1,13 @@
-import React, {useEffect} from 'react';
-import {View, StyleSheet, Text, Button} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, StyleSheet, Text, Button, Switch} from 'react-native';
 import MQTTConnection from './MQTTConnection';
 import {Buffer} from 'buffer';
+import ProgressCircle from 'react-native-progress-circle';
 global.Buffer = Buffer;
 
 export default function App() {
-
+  const [switchValue, setSwitch] = useState(false);
+  const [lightIntensity, setIntensity] = useState(200);
   useEffect(() => {
     this.mqttConnect = new MQTTConnection();
     this.mqttConnect.onMQTTConnect = this.onMQTTConnect;
@@ -30,8 +32,12 @@ export default function App() {
         'App onMQTTMessageArrived payLoadString: ',
         message.payloadString,
       );
+      if (JSON.parse(message.payloadString)[0].device_id == 'Light') {
+        setIntensity(JSON.parse(message.payloadString)[0].values[0]);
+        console.log(lightIntensity);
+      }
     };
-    
+
     onMQTTMessageDelivered = message => {
       console.log('App onMQTTMessageDelivered', message);
     };
@@ -39,23 +45,58 @@ export default function App() {
     return () => {
       this.mqttConnect.close();
     };
-  }, []);
+  }, [lightIntensity]);
+
+  renderCircle = () => {
+    return (
+      <ProgressCircle
+        style={styles.circle}
+        percent={lightIntensity / 1024 * 100}
+        radius={100}
+        borderWidth={8}
+        color="#3399FF"
+        shadowColor="#999"
+        bgColor="#fff">
+        <Text style={{fontSize: 18}}>{lightIntensity}/1024</Text>
+      </ProgressCircle>
+    );
+  };
 
   return (
     <View styles={styles.container}>
-      <Text>react_native_mqtt</Text>
-      <Button
-        title="Press me"
-        onPress={() => this.mqttConnect.send('khoa', "Hello from the other side")}
-      />
+      <View style={styles.switchContainer}>
+        {this.renderCircle()}    
+        <Switch
+          style={styles.switch}
+          onValueChange={value => {
+            setSwitch(value);
+            this.mqttConnect.send(
+              'khoa',
+              `[{ "device_id": "LightD", "values": ["${switchValue ? 0 : 1}", "255"]}]`
+            );
+          }}
+          value={switchValue}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  switchContainer: {
+    marginTop: 100,
+    alignSelf: 'center',
+  },
+  circle: {
+    marginTop: 200,
+  },
+  switch: {
+    marginTop: 50,
+    alignSelf: 'center',
+    transform: [{scaleX: 2}, {scaleY: 1.8}],
+    paddingTop: 40,
   },
 });
