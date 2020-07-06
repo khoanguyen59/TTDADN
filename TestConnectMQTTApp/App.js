98663df4a60f32ea5938a9df22e25b15247a8191
kietran99 <kietran99@gmail.com>
2020-07-07 00:11:27 +0700

    Replace websocket MQTT API with TCP MQTT API

--- TestConnectMQTTApp/App.js
+++ TestConnectMQTTApp/App.js
@@ -1,63 +1,110 @@
 import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, Text, Button, Switch} from 'react-native';
-import MQTTConnection from './src/MQTTConnection';
+import {View, StyleSheet, Text, Switch} from 'react-native';
+//import MQTTConnection from './src/MQTTConnection';
 import {Buffer} from 'buffer';
 import ProgressCircle from 'react-native-progress-circle';
 global.Buffer = Buffer;
+import uuid from 'react-native-uuid';
+import MQTT from 'react-native-mqtt-angelos3lex';
 
-const brokerAddr = 'broker.mqttdashboard.com';
-const brokerPort = 8000;
-const subScribeTopic = 'khoa';
-const publishTopic = 'khoa';
+//const brokerAddr = 'broker.mqttdashboard.com';
+//const brokerPort = 8000;
 
-//const brokerAddr = 'tcp://13.76.250.158';
+//const brokerAddr = '52.187.125.59';
 //const brokerPort = 1883;
-//const subScribeTopic = 'Topic/Light';
-//const publishTopic = 'Topic/LightD';
+const subscribeTopic = 'Topic/Light';
+const publishTopic = 'Topic/LightD';
+const myUserName = 'BKvm';
+const myPassword = 'Hcmut_CSE_2020';
+const qos = 2;
+
+//const uri = 'mqtt://test.mosquitto.org:1883';
+const uri = 'mqtt://52.187.125.59:1883'
+//const uri = 'mqtt://broker.hivemq.com:1883';
 
 export default function App() {
+
   const [switchValue, setSwitch] = useState(false);
   const [lightIntensity, setIntensity] = useState(200);
+
+  // useEffect(() => {
+  //   this.mqttConnect = new MQTTConnection();
+  //   this.mqttConnect.onMQTTConnect = this.onMQTTConnect;
+  //   this.mqttConnect.onMQTTLost = this.onMQTTLost;
+  //   this.mqttConnect.onMQTTMessageArrived = this.onMQTTMessageArrived;
+  //   this.mqttConnect.onMQTTMessageDelivered = this.onMQTTMessageDelivered;
+
+  //   this.mqttConnect.connect(brokerAddr, brokerPort);
+
+  //   onMQTTConnect = () => {
+  //     console.log('App onMQTTConnect');
+  //     this.mqttConnect.subscribeChannel(subscribeTopic);
+  //   };
+
+  //   onMQTTLost = () => {
+  //     console.log('App onMQTTLost');
+  //   };
+
+  //   onMQTTMessageArrived = message => {
+  //     console.log('App onMQTTMessageArrived', message);
+  //     console.log(
+  //       'App onMQTTMessageArrived payLoadString: ',
+  //       message.payloadString,
+  //     );
+  //     if (JSON.parse(message.payloadString)[0].device_id == 'Nhom 3') {
+  //       setTimeout(() => {
+  //         setIntensity(Number(JSON.parse(message.payloadString)[0].values[0]));
+  //       }, 1000);
+  //     }
+  //     console.log(lightIntensity);
+  //   };
+
+  //   onMQTTMessageDelivered = message => {
+  //     console.log('App onMQTTMessageDelivered', message);
+  //   };
+
+  //   return () => {
+  //     this.mqttConnect.close();
+  //   };
+  // }, [lightIntensity]);
+
+  //----------------------------------//;
+
+  const currentTime = +new Date();
+  let clientID = currentTime + uuid.v1();
+  clientID = clientID.slice(0, 23);
+  console.log('clientID:', clientID);
+  
   useEffect(() => {
-    this.mqttConnect = new MQTTConnection();
-    this.mqttConnect.onMQTTConnect = this.onMQTTConnect;
-    this.mqttConnect.onMQTTLost = this.onMQTTLost;
-    this.mqttConnect.onMQTTMessageArrived = this.onMQTTMessageArrived;
-    this.mqttConnect.onMQTTMessageDelivered = this.onMQTTMessageDelivered;
+    const appClient = MQTT.createClient({
+      uri: uri,
+      clientId: clientID,
+      user: myUserName,
+      pass: myPassword,
+    }).then((client) => {
+        client.on('closed', function () {
+          console.log('mqtt.event.closed');
+        });
 
-    this.mqttConnect.connect(brokerAddr, brokerPort);
+        client.on('error', function (msg) {
+          console.log('mqtt.event.error', msg);
+        });
 
-    onMQTTConnect = () => {
-      console.log('App onMQTTConnect');
-      this.mqttConnect.subscribeChannel(subScribeTopic);
-    };
+        client.on('message', function (msg) {
+          console.log('mqtt.event.message', msg);
+        });
 
-    onMQTTLost = () => {
-      console.log('App onMQTTLost');
-    };
+        client.on('connect', function () {
+          console.log('Connected to: ' + uri);
+          client.subscribe(subscribeTopic, qos);
+          client.publish(subscribeTopic, JSON.parse(JSON.stringify(`[{ "device_id": "Nhom 3", "values": ["${switchValue ? 1 : 0}", "255"]}]`)), qos, false);
+        });
 
-    onMQTTMessageArrived = message => {
-      console.log('App onMQTTMessageArrived', message);
-      console.log(
-        'App onMQTTMessageArrived payLoadString: ',
-        message.payloadString,
-      );
-      if (JSON.parse(message.payloadString)[0].device_id == 'Light') {
-        setTimeout(() => {
-          setIntensity(Number(JSON.parse(message.payloadString)[0].values[0]));
-        }, 1000);
-      }
-      console.log(lightIntensity);
-    };
-
-    onMQTTMessageDelivered = message => {
-      console.log('App onMQTTMessageDelivered', message);
-    };
-
-    return () => {
-      this.mqttConnect.close();
-    };
-  }, [lightIntensity]);
+        client.connect();
+      }).catch(function (err) {
+          console.log(err);
+      });
+  }, [switchValue]);
 
   renderCircle = () => {
     return (
@@ -69,7 +116,7 @@ export default function App() {
         color="#3399FF"
         shadowColor="#999"
         bgColor="#fff">
-        <Text style={{fontSize: 18}}>{lightIntensity}/1024</Text>
+        <Text style={{fontSize: 18}}>{lightIntensity}/1023</Text>
       </ProgressCircle>
     );
   };
@@ -77,15 +124,16 @@ export default function App() {
   return (
     <View styles={styles.container}>
       <View style={styles.switchContainer}>
-        {this.renderCircle()}
+        {renderCircle()}
         <Switch
           style={styles.switch}
           onValueChange={value => {
             setSwitch(value);
-            this.mqttConnect.send(
-              publishTopic,
-              `[{ "device_id": "LightD", "values": ["${switchValue ? 0 : 1}", "255"]}]`
-            );
+            //client.publish(publishTopic, [{ "device_id": "Nhom 3", "values": ["${switchValue ? 0 : 1}", "255"]}], qos, false);
+            // this.mqttConnect.send(
+            //   publishTopic,
+            //   `[{ "device_id": "Nhom 3", "values": ["${switchValue ? 0 : 1}", "255"]}]`
+            // );
           }}
           value={switchValue}
         />
--- TestConnectMQTTApp/src/MQTTConnection.js
+++ TestConnectMQTTApp/src/MQTTConnection.js
@@ -1,117 +1,118 @@
-import init from 'react_native_mqtt';
-import uuid from 'react-native-uuid';
-import {AsyncStorage} from 'react-native';
+// import init from 'react_native_mqtt';
+// import uuid from 'react-native-uuid';
+// import {AsyncStorage} from 'react-native';
 
-const myUserName = 'yourUser';
-const myPassword = 'yourPass';
+// //const myUserName = 'yourUser';
+// //const myPassword = 'yourPass';
 
-//const myUserName = 'BKvm2';
-//const myPassword = 'Hcmut_CSE_2020';
+// const myUserName = 'BKvm';
+// const myPassword = 'Hcmut_CSE_2020';
 
-init({
-  size: 10000,
-  storageBackend: AsyncStorage,
-  defaultExpires: 1000 * 3600 * 24,
-  enableCache: true,
-  reconnect: true,
-  sync: {},
-});
+// init({
+//   size: 10000,
+//   storageBackend: AsyncStorage,
+//   defaultExpires: 1000 * 3600 * 24,
+//   enableCache: true,
+//   reconnect: true,
+//   sync: {},
+// });
 
-const defaultConnectOptions = {
-  reconnect: true,
-  cleanSession: true,
-  mqttVersion: 3,
-  keepAliveInterval: 60,
-  timeout: 60,
-};
+// const defaultConnectOptions = {
+//   reconnect: true,
+//   cleanSession: true,
+//   mqttVersion: 3,
+//   keepAliveInterval: 60,
+//   timeout: 60,
+// };
 
-export default class MQTTConnection {
-  constructor() {
-    this.mqtt = null;
-    this.QOS = 0;
-    this.RETAIN = true;
-  }
+// export default class MQTTConnection {
+//   constructor() {
+//     this.mqtt = null;
+//     this.QOS = 0;
+//     this.RETAIN = true;
+//   }
 
-  connect(host, port, options = null) {
-    if (options) {
-      this.QOS = options.qos;
-      this.RETAIN = options.retain;
-    }
+//   connect(host, port, options = null) {
+//     if (options) {
+//       this.QOS = options.qos;
+//       this.RETAIN = options.retain;
+//     }
 
-    let currentTime = +new Date();
-    let clientID = currentTime + uuid.v1();
-    clientID = clientID.slice(0, 23);
-    console.log('clientID:', clientID);
+//     let currentTime = +new Date();
+//     let clientID = currentTime + uuid.v1();
+//     clientID = clientID.slice(0, 23);
+//     console.log('clientID:', clientID);
 
-    this.mqtt = new Paho.MQTT.Client(host, port, clientID);
-    this.mqtt.onConnectionLost = res => {
-      this.onMQTTLost;
-    };
-    this.mqtt.onMessageArrived = (message) => {
-      this.onMQTTMessageArrived(message);
-    };
-    this.mqtt.onMessageDelivered = (message) => {
-      this.onMQTTMessageDelivered(message);
-    };
-    const connectOptions = options ? options : defaultConnectOptions;
+//     this.mqtt = new Paho.MQTT.Client(host, port, clientID);
+//     this.mqtt.onConnectionLost = res => {
+//       this.onMQTTLost;
+//     };
+//     this.mqtt.onMessageArrived = (message) => {
+//       this.onMQTTMessageArrived(message);
+//     };
+//     this.mqtt.onMessageDelivered = (message) => {
+//       this.onMQTTMessageDelivered(message);
+//     };
+//     const connectOptions = options ? options : defaultConnectOptions;
 
-    this.mqtt.connect({
-      onSuccess: this.onMQTTSuccess,
-      useSSL: false,
-      userName: myUserName,
-      password: myPassword,
-      onFailure: this.onMQTTFailure,
-      ...connectOptions,
-    });
-  }
+//     this.mqtt.connect({
+//       onSuccess: this.onMQTTSuccess,
+//       useSSL: false,
+//       userName: myUserName,
+//       password: myPassword,
+//       onFailure: this.onMQTTFailure,
+//       ...connectOptions,
+//     });
+//   }
 
-  onMQTTSuccess = () => {
-    this.onMQTTConnect();
-  };
+//   onMQTTSuccess = () => {
+//     console.log('Connected Successfully!')
+//     this.onMQTTConnect();
+//   };
 
-  onMQTTFailure = () => {
-    console.log('App onMQTTLost');
-    this.onMQTTLost;
-  };
+//   onMQTTFailure = () => {
+//     console.log('Connected Failed');
+//     this.onMQTTLost;
+//   };
 
-  subscribeChannel(channel) {
-    console.log('MQTTConnection subscribeChannel: ', channel);
-    if (!this.mqtt || !this.mqtt.isConnected()) {
-      return;
-    }
-    this.mqtt.subscribe(channel, this.QOS);
-  }
+//   subscribeChannel(channel) {
+//     console.log('MQTTConnection subscribeChannel: ', channel);
+//     if (!this.mqtt || !this.mqtt.isConnected()) {
+//       return;
+//     }
+//     this.mqtt.subscribe(channel, this.QOS);
+//   }
 
-  unsubscribeChannel(channel) {
-    console.log('MQTTConnection unsubscribeChannel: ', channel);
-    if (!this.mqtt || !this.mqtt.isConnected()) {
-      return;
-    }
-    this.mqtt.unsubscribe(channel);
-  }
+//   unsubscribeChannel(channel) {
+//     console.log('MQTTConnection unsubscribeChannel: ', channel);
+//     if (!this.mqtt || !this.mqtt.isConnected()) {
+//       return;
+//     }
+//     this.mqtt.unsubscribe(channel);
+//   }
 
-  send(channel = null, payload) {
-    console.log('MQTTConnection send:');
-    if (!this.mqtt || !this.mqtt.isConnected()) {
-      return;
-    }
+//   send(channel = null, payload) {
+//     console.log('MQTTConnection send:');
+//     if (!this.mqtt || !this.mqtt.isConnected()) {
+//       return;
+//     }
 
-    if (!channel || !payload) {
-      return false;
-    }
-    console.log(
-      `MQTTConnection send publish channel', ${channel}, payload: ${payload} qos: ${this.QOS} retain: ${this.RETAIN}`,
-    );
-    this.mqtt.publish(channel, payload, this.QOS, this.RETAIN);
-  }
+//     if (!channel || !payload) {
+//       return false;
+//     }
+//     console.log(
+//       `MQTTConnection send publish channel', ${channel}, payload: ${payload} qos: ${this.QOS} retain: ${this.RETAIN}`,
+//     );
+//     this.mqtt.publish(channel, payload, this.QOS, this.RETAIN);
+//   }
 
-  close() {
-    this.mqtt && this.mqtt.disconnect();
-    this.mqtt = null;
-  }
-}
+//   close() {
+//     this.mqtt && this.mqtt.disconnect();
+//     this.mqtt = null;
+//   }
+// }
 
-MQTTConnection.prototype.onMQTTConnect = null;
-MQTTConnection.prototype.onMQTTLost = null;
-MQTTConnection.prototype.onMQTTMessageArrived = null;
-MQTTConnection.prototype.onMQTTMessageDelivered = null;
+// MQTTConnection.prototype.onMQTTConnect = null;
+// MQTTConnection.prototype.onMQTTLost = null;
+// MQTTConnection.prototype.onMQTTMessageArrived = null;
+// MQTTConnection.prototype.onMQTTMessageDelivered = null;
