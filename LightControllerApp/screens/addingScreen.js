fd964295cf7d0815a7ef47c4a6d29194bca12270
kietran99 <kietran99@gmail.com>
2020-07-10 20:36:44 +0700

    Merge Adding screen

--- LightControllerApp/screens/addingScreen.js
+++ LightControllerApp/screens/addingScreen.js
@@ -1,24 +1,10 @@
-<<<<<<< HEAD
-import React, {useState} from 'react';
-=======
 import React, {useState, useEffect} from 'react';
->>>>>>> AddDevice_homescreen_fix
 import {
   StyleSheet,
   TextInput,
   View,
   Text,
   TouchableOpacity,
-<<<<<<< HEAD
-  Image,
-  ToastAndroid,
-} from 'react-native';
-import ScreenTemplate from './screenTemplate';
-import NavigationTab from '../components/navigationTab';
-import DeviceListItem from '../custom_components/deviceListItem';
-import {selectedRoom} from '../screens/homeScreen.js';
-import * as firebase from 'firebase';
-=======
   ToastAndroid,
   Dimensions,
 } from 'react-native';
@@ -29,7 +15,6 @@ import {selectedRoom} from '../screens/homeScreen.js';
 import {globalStyles} from '../styles/global';
 import * as firebase from 'firebase';
 import { set } from 'react-native-reanimated';
->>>>>>> AddDevice_homescreen_fix
 
 const firebaseConfig = {
   apiKey: 'AIzaSyADawFZYkBiSUoh5bdWpescXF0V2DvDvvk',
@@ -41,23 +26,6 @@ const firebaseConfig = {
   appId: '1:670980151251:web:245ac428bec24de86a0126',
 };
 
-<<<<<<< HEAD
-const screenIdx = 2;
-
-async function numChildCount(room) {
-  const snapshot = await firebase
-    .database()
-    .ref('/deviceList/' + room)
-    .once('value');
-  return snapshot.numChildren();
-}
-
-export default function addingScreen({navigation}) {
-  //TODO
-  const [deviceName, setName] = useState('');
-  const [deviceState, setState] = useState(false);
-  const [deviceType, setType] = useState('');
-=======
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
@@ -72,45 +40,10 @@ export default function addingScreen({navigation}) {
   const [deviceName, setDeviceName] = useState('');
   const [deviceState, setDeviceState] = useState(false);
   const [deviceType, setDeviceType] = useState('');
->>>>>>> AddDevice_homescreen_fix
 
   const [nameError, setNameErr] = useState(false);
   const [stateError, setStateErr] = useState(false);
   const [typeError, setTypeErr] = useState(false);
-<<<<<<< HEAD
-
-  async function onConfirm() {
-    var currentCount = await numChildCount(selectedRoom);
-
-    if (deviceName === '') {
-      setNameErr(true);
-    } else if (deviceType !== 'Light' && deviceType !== 'Sensor') {
-      setTypeErr(true);
-    } else if (
-      (deviceType === 'Light' &&
-        (deviceState !== 'true' && deviceState !== 'false')) ||
-      (deviceType === 'Sensor' && isNaN(deviceState))
-    ) {
-      setStateErr(true);
-    } else {
-      firebase
-        .database()
-        .ref('deviceList/' + selectedRoom)
-        .child(currentCount)
-        .set({
-          deviceID: currentCount + 1,
-          deviceName: deviceName,
-          deviceState: deviceType === 'Sensor' ? deviceState : deviceState === 'true',
-          deviceType: deviceType,
-        });
-      navigation.navigate('Device');
-    }
-  }
-
-  function nameErrorRender() {
-    return (
-      <Text style={styles.errorText}>Tên thiết bị không được để trống</Text>
-=======
   const [seletedType, setSeletedType] = useState('');
   const [seletedState, setSeletedState] = useState('');
   const [currentCount, setCurrentCount] = useState(0);
@@ -171,65 +104,18 @@ export default function addingScreen({navigation}) {
   function nameErrorRender() {
     return (
       <Text style={styles.errorText}>Device name must not be empty!</Text>
->>>>>>> AddDevice_homescreen_fix
     );
   }
 
   function typeErrorRender() {
     return (
       <Text style={styles.errorText}>
-<<<<<<< HEAD
-        Kiểu thiết bị chỉ có thể là Light hoặc Sensor
-=======
         Please choose a device type!
->>>>>>> AddDevice_homescreen_fix
       </Text>
     );
   }
 
   function stateErrorRender() {
-<<<<<<< HEAD
-    return (
-      <Text style={styles.errorText}>
-        Nhập số nguyên trong khoảng 0-1023 nếu kiểu thiết bị là Sensor,
-        true/false nếu kiểu thiết bị là Light
-      </Text>
-    );
-  }
-
-  return (
-    <View style={styles.searchContainer}>
-      <View style={styles.textInputContainer}>
-        <TextInput
-          style={styles.textInput}
-          onChangeText={name => setName(name)}
-          placeholder="Device Name"
-        />
-        {nameError && nameErrorRender()}
-      </View>
-      <View style={styles.textInputContainer}>
-        <TextInput
-          style={styles.textInput}
-          onChangeText={state => setState(state)}
-          placeholder="Device State"
-        />
-        {stateError && stateErrorRender()}
-      </View>
-      <View style={styles.textInputContainer}>
-        <TextInput
-          style={styles.textInput}
-          onChangeText={type => setType(type)}
-          placeholder="Device Type"
-        />
-        {typeError && typeErrorRender()}
-      </View>
-      <TouchableOpacity style={styles.saveButton}>
-        <Text style={styles.saveText} onPress={() => onConfirm()}>
-          Add New Device
-        </Text>
-      </TouchableOpacity>
-    </View>
-=======
     if(deviceType === LIGHT_TYPE){
       return (
         <Text style={styles.errorText}>
@@ -334,23 +220,10 @@ export default function addingScreen({navigation}) {
             </View>
           }
         />          
->>>>>>> AddDevice_homescreen_fix
   );
 }
 
 const styles = StyleSheet.create({
-<<<<<<< HEAD
-  textInputContainer: {
-    marginTop: 20,
-    height: 60,
-    padding: 10,
-  },
-  textInput: {
-    backgroundColor: 'white',
-    borderWidth: 1,
-    borderColor: 'black',
-    borderRadius: 30,
-=======
   searchContainer:{
     bottom: -20
   },
@@ -368,7 +241,6 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     paddingStart: 20,
     paddingEnd: 20,
->>>>>>> AddDevice_homescreen_fix
   },
   image: {
     width: 'auto',
@@ -376,38 +248,23 @@ const styles = StyleSheet.create({
     resizeMode: 'contain',
   },
   saveButton: {
-<<<<<<< HEAD
-    backgroundColor: '#2095f3',
-    position: 'absolute',
-    height: 50,
-    width: 150,
-    alignSelf: 'center',
-    marginTop: 250,
-=======
     ...globalStyles.alternativeColor,
     position: 'absolute',
     height: 40,
     width: 150,
     alignSelf: 'center',
     marginTop: 240,
->>>>>>> AddDevice_homescreen_fix
     borderRadius: 30,
     justifyContent: 'center',
   },
   saveText: {
-<<<<<<< HEAD
-    fontSize: 18,
-=======
     ...globalStyles.regularText,
->>>>>>> AddDevice_homescreen_fix
     alignSelf: 'center',
     color: 'white',
   },
   errorText: {
     color: 'red',
   },
-<<<<<<< HEAD
-=======
   textSensorValue:{
     padding: 10, 
     width : 200,
@@ -418,5 +275,4 @@ const styles = StyleSheet.create({
     alignSelf : 'flex-end',
     bottom: 25,
   }
->>>>>>> AddDevice_homescreen_fix
 });
