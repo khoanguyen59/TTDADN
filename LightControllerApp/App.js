0212fbd67fba3e6503787a1de4c4ddae33ebfd9b
Nguyễn Trần Phương Khoa <phuongkhoa95ntp@gmail.com>
2020-07-11 14:13:54 +0700

    Fix Header & LogList

--- LightControllerApp/App.js
+++ LightControllerApp/App.js
@@ -1,5 +1,7 @@
+/* eslint-disable prettier/prettier */
 import 'react-native-gesture-handler';
 import React, { useEffect } from 'react';
+import {Platform, InteractionManager, StyleSheet, Image} from 'react-native';
 import {NavigationContainer, DarkTheme, DefaultTheme} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import homeScreen from './screens/homeScreen';
@@ -11,6 +13,7 @@ import addingScreen from './screens/addingScreen';
 import timerScreen from './screens/timerScreen';
 import addRoom from './screens/addRoomScreen';
 import SplashScreen from 'react-native-splash-screen';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 
 import * as firebase from 'firebase';
@@ -27,6 +30,50 @@ const firebaseConfig = {
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
+}
+
+const _setTimeout = global.setTimeout;
+const _clearTimeout = global.clearTimeout;
+const MAX_TIMER_DURATION_MS = 60 * 1000;
+if (Platform.OS === 'android') {
+    // Work around issue `Setting a timer for long time`
+    // see: https://github.com/firebase/firebase-js-sdk/issues/97
+    const timerFix = {};
+    const runTask = (id, fn, ttl, args) => {
+        const waitingTime = ttl - Date.now();
+        if (waitingTime <= 1) {
+            InteractionManager.runAfterInteractions(() => {
+                if (!timerFix[id]) {
+                    return;
+                }
+                delete timerFix[id];
+                fn(...args);
+            });
+            return;
+        }
+
+        const afterTime = Math.min(waitingTime, MAX_TIMER_DURATION_MS);
+        timerFix[id] = _setTimeout(() => runTask(id, fn, ttl, args), afterTime);
+    };
+
+    global.setTimeout = (fn, time, ...args) => {
+        if (MAX_TIMER_DURATION_MS < time) {
+            const ttl = Date.now() + time;
+            const id = '_lt_' + Object.keys(timerFix).length;
+            runTask(id, fn, ttl, args);
+            return id;
+        }
+        return _setTimeout(fn, time, ...args);
+    };
+
+    global.clearTimeout = id => {
+        if (typeof id === 'string' && id.startsWith('_lt_')) {
+            _clearTimeout(timerFix[id]);
+            delete timerFix[id];
+            return;
+        }
+        _clearTimeout(id);
+    };
 }
 
 function readUserData() {
@@ -48,27 +95,43 @@ const MyTheme = {
   },
 };
 const AppNavigator = createStackNavigator();
+const screenOption = navigation => ({
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigation.navigate('Home');
+      }}>
+      <Image source={require('./icons/homeIcon.png')} style={styles.image} />
+    </TouchableOpacity>
+  ),
+});
 const App = () => {
   useEffect(()=>{
     SplashScreen.hide();
   },[])
   readUserData();
   return (
-    <NavigationContainer
-      theme = {MyTheme}
-      >
+    <NavigationContainer theme = {MyTheme}>
       <AppNavigator.Navigator>
         <AppNavigator.Screen name="Home" component={homeScreen} />
-        <AppNavigator.Screen name="AddRoom" component={addRoom} />
-        <AppNavigator.Screen name="Device" component={deviceScreen} />
-        <AppNavigator.Screen name="Setting" component={settingScreen} />
-        <AppNavigator.Screen name="SetTimer" component={setTimerScreen} />
-        <AppNavigator.Screen name="History" component={historyScreen} />
-        <AppNavigator.Screen name="Adding" component={addingScreen} />
-        <AppNavigator.Screen name="Timer" component={timerScreen} />
-        
+        <AppNavigator.Screen options={screenOption} name="AddRoom" component={addRoom} />
+        <AppNavigator.Screen options={screenOption} name="Device" component={deviceScreen} />
+        <AppNavigator.Screen options={screenOption} name="Setting" component={settingScreen} />
+        <AppNavigator.Screen options={screenOption} name="SetTimer" component={setTimerScreen} />
+        <AppNavigator.Screen options={screenOption}name="History" component={historyScreen} />
+        <AppNavigator.Screen options={screenOption} name="Adding" component={addingScreen} />
+        <AppNavigator.Screen options={screenOption} name="Timer" component={timerScreen} />
       </AppNavigator.Navigator>
     </NavigationContainer>
   );
 };
 export default App;
+
+const styles = StyleSheet.create({
+  image: {
+    //...StyleSheet.absoluteFillObject,
+    width: 50,
+    height: 50,
+    resizeMode: 'contain',
+  },
+});
